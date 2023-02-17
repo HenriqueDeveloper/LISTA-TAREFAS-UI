61b99bb6d156c23b257e8f00ff2a6ad224f4444d
Carlos Henrique <carlossoaressantana@hotmail.com>
2023-02-17 09:01:11 -0300

    feat: first commit

---
+++ db/conn.js
@@ -0,0 +1,20 @@
+const { Sequelize } = require('sequelize')
+
+require('dotenv').config()
+
+const sequelize = new Sequelize(
+  process.env.DB_NAME, 
+  process.env.DB_USER, 
+  process.env.DB_PASSWORD, {
+  host: process.env.DB_HOST,
+  dialect: 'mysql',
+})
+
+try {
+  sequelize.authenticate()
+  console.log('Conectamos com o Sequelize!')
+} catch (error) {
+  console.error('Não foi possível conectar:', error)
+}
+
+module.exports = sequelize
